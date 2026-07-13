6fb0615ca8ecf17dd001b522476efb3da73fb77c
Dhruv Sharma <ds861356@gmail.com>
2026-07-13 16:46:40 +0530

    Connect frontend with Render backend link

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,8 +2,7 @@ import axios from "axios";
 import Approutes from "./routes/routes";
 import { ToastContainer } from "react-toastify";
 import { AuthProvider } from "./context/AuthContext";
-axios.defaults.baseURL = "http://localhost:5000/";
-
+axios.defaults.baseURL = "https://erms-rjhx.onrender.com/";
 
 const App = () => {
   return (
